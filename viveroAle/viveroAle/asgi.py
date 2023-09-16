9969963fb140752fd083c4e06f8ec9cfe990685c
Agustin <agustin.ribotta.r@gmail.com>
2023-09-16 11:03:05 +0200

    ImganProtda

--- viveroAle/viveroAle/asgi.py
+++ viveroAle/viveroAle/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveroAle.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveroAle.settings.local')
 
 application = get_asgi_application()
--- viveroAle/viveroAle/wsgi.py
+++ viveroAle/viveroAle/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveroAle.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveroAle.settings.local')
 
 application = get_wsgi_application()
